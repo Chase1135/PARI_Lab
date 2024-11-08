ba5d4a42d1482f7a273eab053793441fcdb5f99e
Chase1135 <cna5194@psu.edu>
2024-11-07 23:44:53 -0500

    testing

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ ip = '0.0.0.0'
 port = 5000
 server_socket = socket(AF_INET, SOCK_STREAM)
 
-print("Binding to IP and port...")
+print("Binding to IP and port...", flush=True)
 server_socket.bind((ip, port))
-print("Bound successfully.")
+print("Bound successfully.", flush=True)
 
-print("Listening for incoming connections...")
+print("Listening for incoming connections...", flush=True)
 server_socket.listen(1)
-print("Ready to receive connections.")
+print("Ready to receive connections.", flush=True)
 print(f"Listening on {ip}:{port}", flush=True)
 
 print("testing the automated builds", flush=True)
