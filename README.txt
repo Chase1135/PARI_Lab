e80634037151053ed20ecda5729de4839ae00983
Chase1135 <cna5194@psu.edu>
2024-11-15 13:30:54 -0500

    test_connection updated, readme fixed

--- README.txt
+++ README.txt
@@ -1,12 +1,13 @@
 HOW TO RUN WITHIN DOCKER DESKTOP:
 1. open docker terminal
-2. pull docker image ('docker pull chasea1135/pari-lab-server:latest')
-3. run container ('docker run -p 5000:5000 chasea1135/pari-lab-server:latest')
-4. enter host machine's IP as environment variable
+2. pull docker image ('docker pull chasea1135/pari_lab_server:latest')
+3. run container ('docker run -p 5000:5000 chasea1135/pari_lab_server:latest') OR click run on downloaded image
+4. if run from images tab, enter port # (5000)
 
-1. clone repository, install necessary dependencies (pydadntic, fastapi, uvicorn)
+RUNNING LOCALLY:
+1. clone repository, install necessary dependencies (pydantic, fastapi, uvicorn, websockets)
 2. open docker terminal, navigate to project folder
-3. type 'docker-compose up' in terminal
+3. type 'docker-compose up --build' in terminal
 
 TO TEST:
-Invoke-RestMethod -Uri "http://'insertiphere':5000/textual" -Method Post -Headers @{ "Content-Type" = "application/json" } -Body '{"sample": "textual data"}'
+1. run test_connection.py (type exit to close connection)
--- test_connection.py
+++ test_connection.py
@@ -2,22 +2,18 @@ import asyncio
 import websockets
 
 async def test_connection():
-    uri = "ws://localhost:5000/ws/textual"  # Change to your WebSocket endpoint
+    uri = "ws://192.168.1.125:5000/ws/textual" 
     async with websockets.connect(uri) as websocket:
         while True:
-            # Ask for user input or send a message automatically
             message = input("Enter message to send (or 'exit' to quit): ")
             
-            # If the user types 'exit', break the loop and close the connection
+            await websocket.send(f'{{"message": "{message}"}}')
+            
+            response = await websocket.recv()
+            print(f"Response: {response}")
+
             if message.lower() == 'exit':
                 print("Exiting...")
                 break
-            
-            # Send the message
-            await websocket.send(f'{{"message": "{message}"}}')
-            
-            # Receive and print the response
-            response = await websocket.recv()
-            print(f"Response: {response}")
 
 asyncio.run(test_connection())
